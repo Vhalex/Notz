a32f92fa2717fd029f0eaae43128ee33b426b857
valentina <valemat248@gmail.com>
2018-04-19 18:55:59 +0200

    4connect

--- app/src/main/java/eu/fse/notz/MainActivity.java
+++ app/src/main/java/eu/fse/notz/MainActivity.java
@@ -1,7 +1,11 @@
 package eu.fse.notz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AlertDialogLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
@@ -15,18 +19,32 @@ import eu.fse.notz.R;
  * Created by Amministratore on 12/04/2018.
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     //private String[] myDataset={"nota1", "nota2"};
     private ArrayList<Note> myDataset;
+    private FloatingActionButton fab;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener(){
+
+
+            @Override
+            public void onClick(View v) {
+            showDialod();
+            }
+                               });
+
 
         mRecyclerView= (RecyclerView) findViewById(R.id.notes_rv);
         mRecyclerView.setHasFixedSize(true);
@@ -43,4 +61,14 @@ public class MainActivity extends AppCompatActivity {
         mAdapter = new NotesAdapter(myDataset);
         mRecyclerView.setAdapter(mAdapter);
     }
+
+  private void showDialod(){
+      AlertDialog.Builder builder = new AlertDialog.Builder(this);
+      builder.setView(R.layout.activity_dialog);
+      builder.setTitle(R.string.title_d);
+
+      builder.setPositiveButton(R.string.)
+      AlertDialog.show();
+
+  }
 }
